076c36e154564e2c7541ece3e88af5d141bcb827
Eduardo Amorim <eduardoaamorim0@gmail.com>
2021-03-09 12:40:18 -0300

    resposta 4 pergunta

--- Exercicio_Sem1.py
+++ Exercicio_Sem1.py
@@ -4,15 +4,18 @@ dataset = pd.read_csv( 'datasets/kc_house_data.csv' )
 
 # 1 pergunta
 
-print(len(dataset))
+#print(len(dataset))
 
 # 2 pergunta
 
-print(len(dataset.columns))
+#print(len(dataset.columns))
 
 # 3 pergunta
 
-print(dataset.columns)
+#print(dataset.columns)
 
 #  4 pergunta
 
+print(dataset.max())
+
+# 5 pergunta
