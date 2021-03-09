33503a86dcbeb7272c699badabe4e2b40acd1333
Eduardo Amorim <eduardoaamorim0@gmail.com>
2021-03-09 10:11:23 -0300

    ajeitado import

--- ex_01.py
+++ ex_01.py
@@ -1,7 +1,5 @@
 
 import pandas as pd
 
-#Carregando o banco de dados que baixamos no kaggle
-
-data = pd.read_csv( 'C:/Users/PICHAU/Desktop/AnaliseDeDados/DataAnalysis/datasets/kc_house_data' )
+data = pd.read_csv( 'datasets/kc_house_data.csv' )
 
