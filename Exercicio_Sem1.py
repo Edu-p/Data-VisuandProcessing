4936164db8222fc104f4afe41b94825aff949595
Eduardo Amorim <eduardoaamorim0@gmail.com>
2021-03-10 14:33:45 -0300

    pergunta 6

--- Exercicio_Sem1.py
+++ Exercicio_Sem1.py
@@ -4,20 +4,25 @@ dataset = pd.read_csv( 'datasets/kc_house_data.csv' )
 
 # 1 pergunta
 
-print(len(dataset))
+#print(len(dataset))
 
 # 2 pergunta
 
-print(len(dataset.columns))
+#print(len(dataset.columns))
 
 # 3 pergunta
 
-print(dataset.columns)
+#print(dataset.columns)
 
 #  4 pergunta
 
-print(dataset.max())
+#print(dataset.max())
 
 # 5 pergunta
 
-print(dataset.sort_values('bedrooms').max())
+#print(dataset.sort_values('bedrooms').max())
+
+# 6 pergunta
+
+print( dataset['bedrooms'].sum())
+
