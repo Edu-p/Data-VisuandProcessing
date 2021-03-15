7bedf7e7427378fa4a12eded6c34c3f0f7e8d380
Eduardo Amorim <eduardoaamorim0@gmail.com>
2021-03-15 09:26:46 -0300

    adicionando seleçao de dados parcial

--- notas_aula2.2.py
+++ notas_aula2.2.py
@@ -10,13 +10,44 @@ dataF = pd.read_csv('datasets/kc_house_data.csv')
 # dataF['nomePica'] = "Eduzap"
 # dataF['numero80'] = 80
 # dataF['dataFormat'] = pd.to_datetime('2002-02-20')
-#
+
 # print(dataF[['id','date','nomePica','numero80','dataFormat']].head(3))
 # print(dataF.dtypes)
 
-# print(dataF.columns)
+#print(dataF.columns)
 
 #----------------------
 # deletando variaveis:
 #----------------------
+
+# .drop é a funçao que deleta do dataSet a coluna, tem que armazzenar
+# o resultado em algum detaSet, pode ser ate o mesmo que vc dropou
+
+# print(dataF.columns)
+#
+# dataF = dataF.drop('nomePica',axis=1) # esse axis=1 é deletar todas as linhas ao longo das colunas, depois explikcar direito isso
+#
+# print(dataF.columns)
+
+# cols = ['nomePica','numero80','dataFormat'] # jeito de armazenar uma lista
+#
+# dataF = dataF.drop(cols,axis=1)# isso é uma lista, que a gente vai ver deposi, mas por agora é uma estrutura que guarda dados
+#
+# print(dataF.columns)
+
+#----------------------
+# Selecionando dados:
+#----------------------
+
+## Forma 1:
+
+print(dataF[['price','id','date']].head(2) )
+
+## Forma 2:
+
+#os dados sao dispostos da seguinte forma no pandas
+
+
+
+
 
