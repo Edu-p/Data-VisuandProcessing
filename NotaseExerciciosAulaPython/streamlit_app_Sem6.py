dae5447cc032fcec088e27ee0382bbccdcf8d3e2
Eduardo Amorim <eduardoaamorim0@gmail.com>
2021-03-30 16:37:45 -0300

    interative table 2

--- NotaseExerciciosAulaPython/streamlit_app_Sem6.py
+++ NotaseExerciciosAulaPython/streamlit_app_Sem6.py
@@ -34,7 +34,7 @@ elif(  ( f_zipcode== [] ) & ( f_attributes != [] ) ):
 else:
     data = data.copy()
 
-st.write( data.head(7) )
+# st.write( data.head(7) )
 
 # Avarage metrics
 
@@ -43,4 +43,14 @@ df2 = data[['price','zipcode']].groupby( 'zipcode' ).mean().reset_index()
 df3 = data[['sqft_living','zipcode']].groupby( 'zipcode' ).mean().reset_index()
 df4 = data[['price_m2','zipcode']].groupby( 'zipcode' ).mean().reset_index()
 
-# merge
+# merge metrics
+
+m1 = pd.merge( df1,df2, on='zipcode', how='inner' )# abstrair inner por agora
+m2 = pd.merge( m1,df3, on='zipcode', how='inner' )
+df = pd.merge( m2,df4, on='zipcode', how='inner' )
+
+df.columns = [ 'zipcode', 'total houses', 'price', 'sqft living', 'price/m2' ] # rename coluns
+
+st.dataframe(df, height=600)
+
+# st.write( df.head(7) )
